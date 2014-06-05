8af63b5d5da7feb8403f27354466ce314e79d44b
Dan Grossman <dan@awio.com>
2014-06-05 16:50:26 -0400

    Parent definitions do not always appear before their children in new versions of the browscap definition files, so we now loop through the list twice to ensure the parent properties are in the browsers array before copying them. Also updated the pattern->regex replacements to allow literal plus characters in user agent patterns.

--- browscap.js
+++ browscap.js
@@ -37,7 +37,9 @@ function parse(filename) {
                      .replace(/\-/g, '\\-')
                      .replace(/\*/g, '.*')
                      .replace(/\?/g, '.?')
-            + '$')
+                     .replace(/\+/g, '\\+')
+            + '$'),
+          __pattern__: pattern
         }
         
         browserArray.push(current) // Push new browser object onto array
@@ -50,21 +52,23 @@ function parse(filename) {
         if (value[0] == '"' && value.slice(-1) == '"') {
           value = value.slice(1, -1)
         }
-        
-        if (name != 'Parent') {
-          current[name] = value
-        } else {
-          // Copy properties from the parent's entry
-          var i = patternIndex.lastIndexOf(value)
-          for (var key in browserArray[i]) {
-            if (key != '__regex__' && key != 'Parent') {
-              current[key] = browserArray[i][key]
-            }
-          }
-        }
+
+        current[name] = value
       }
     })
-  
+
+  for (var i = 0; i < browserArray.length; i++) {
+    var current = browserArray[i]
+    if (current.hasOwnProperty('Parent')) {
+      var j = patternIndex.lastIndexOf(current['Parent'])
+      for (var key in browserArray[j]) {
+        if (key != '__regex__' && key != 'Parent' && typeof browserArray[i][key] == 'undefined') {
+          browserArray[i][key] = browserArray[j][key]
+        }
+      }
+    }
+  }
+
   return browserArray
 }
 
@@ -72,7 +76,7 @@ exports.getBrowser = function(userAgent) {
   if (!browsers.length) {
     browsers = parse(inifile)
   }
-  
+
   // Test user agent against each browser regex
   for (var i = 0; i < browsers.length; i++) {
     if (browsers[i].__regex__.test(userAgent)) {
--- test.js
+++ test.js
@@ -1,57 +1,58 @@
-var browscap = require('browscap');
+var browscap = require('./browscap');
 //browscap.setIni('php_browscap.ini');
 
 console.log("Running tests...");
 var passed = 0;
 var failed = 0;
 
-var browser = browscap.getBrowser("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; WinTSI 05.11.2009)");
+var browser = browscap.getBrowser("Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)");
 if (browser['Browser'] == 'IE' && browser['Version'] == '8.0') {
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("Identified " + browser['Browser'] + " " + browser['Version']);
         passed++;
 } else {
         console.log("Failed to identify IE 8.0");
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("-> " + browser['Browser'] + " " + browser['Version']);
         failed++;
 }
 
 var browser = browscap.getBrowser("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.13) Gecko/20100914 Firefox/3.5.13");
 if (browser['Browser'] == 'Firefox' && browser['Version'] == '3.5') {
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("Identified " + browser['Browser'] + " " + browser['Version']);
         passed++;
 } else {
         console.log("Failed to identify Firefox 3.5");
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("-> " + browser['Browser'] + " " + browser['Version']);
         failed++;
 }
 
 var browser = browscap.getBrowser("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.10) Gecko/20100914 Firefox/3.6.10");
 if (browser['Browser'] == 'Firefox' && browser['Version'] == '3.6') {
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("Identified " + browser['Browser'] + " " + browser['Version']);
         passed++;
 } else {
         console.log("Failed to identify Firefox 3.6");
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("-> " + browser['Browser'] + " " + browser['Version']);
         failed++;
 }
 
 var browser = browscap.getBrowser("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.3; WOW64; Trident/7.0; Touch; .NET4.0E; .NET4.0C; .NET CLR 3.5.30729; .NET CLR 2.0.50727; .NET CLR 3.0.30729; Tablet PC 2.0; IPH 1.1.21.4019; ASU2JS)");
 if (browser['Browser'] == 'IE' && browser['Version'] == '11.0') {
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("Identified " + browser['Browser'] + " " + browser['Version']);
         passed++;
 } else {
         console.log("Failed to identify IE 11");
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("-> " + browser['Browser'] + " " + browser['Version']);
         failed++;
 }
 
-var browser = browscap.getBrowser("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3");
-if (browser['Browser'] == 'Chrome' && browser['Version'] == '6.0') {
-        console.log(browser['Browser'] + " " + browser['Version']);
+var browser = browscap.getBrowser("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36");
+if (browser['Browser'] == 'Chrome' && browser['Version'] == '35.0') {
+        console.log("Identified " + browser['Browser'] + " " + browser['Version']);
         passed++;
 } else {
-        console.log("Failed to identify Chrome 6.0");
-        console.log(browser['Browser'] + " " + browser['Version']);
+        console.log("Failed to identify Chrome 35.0");
+        console.log("-> " + browser['Browser'] + " " + browser['Version']);
+	console.log(browser);
         failed++;
 }
 
